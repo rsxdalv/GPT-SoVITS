08649e8244f3412ba003d446b39ecdea2aba8e60
Roberts Slisans <roberts.slisans@gmail.com>
2026-04-22 19:10:29 +0300

    Bump version from 2.6.3 to 2.6.4

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='gpt-sovits',
-    version="2.6.3",
+    version="2.6.4",
     description='GPT-SoVITS text-to-speech synthesis system',
     long_description=long_description,
     long_description_content_type='text/markdown',
